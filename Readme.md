9bb224c00d10e3e6b7c519531196b043743a4628
Markus Rein <markus.rein@infineon.com>
2023-03-24 17:20:46 +0100

    Added protobuf example

--- Readme.md
+++ Readme.md
@@ -1,39 +1,20 @@
-# LDS-006-Lidar-Sensor-Library
+# Dummy branch for development
 
-Folks, from software side of things, [this](https://www.jentsch.io/lds-006-lidar-sensor-reverse-engineering/) is a stub.
+In this branch, no sensor functionality is used. Instead, data is randomly generated and sent to the server.
 
-So I brought i some upgrades. To conduct tests I am using Raspberry Pi Model 3/4. Feel free to use the library with any other hardware.
+Code is in `example/canvas_drawing`
 
-## How to connect hardware
+I compiled message scheme to send data from server to website wih the following command:
+'''
+    protoc --python_out=. msgLDS.proto 
+'''
+using a recent version of protoc*: 
 
-```
-black: GND
-red: VCC (5V)
-blue: UART TX (3v3, Pin 10 @ RPI)
-green: UART RX (3v3, Pin 8 @ RPI)
-```
+'''
+    protoc --version
+    libprotoc 3.21.1
+'''
 
-## How to build library
+*Default installation of ROS-noetic uses version 3.6.1 which is too old to work with recent python library version.
 
-In this directory: setup a Virtual Environment, then install library
-```
-sudo apt-get install python3-venv python3-numpy
-python3 -m venv ./venv --system-site-packages
-source venv/bin/activate
-pip install -r requirements.txt
-python setup.py bdist_wheel
-pip install dist/lds006*.whl
-```
-
-Numpy is used as a system requirement because RPI lacks libraries when installing via pip.
-
-## How to use  library
-
-In this directory: start Virtual Environment, then run 'main.py'.
-
-**Caution: requires access to hardware** Best practice is to add user to group *dialout*.
-
-```
-. venv/bin/activate
-python examples/simple.py /dev/serial0
-```
+...
--- example/canvas_drawing/interactive_canvas.py
+++ example/canvas_drawing/interactive_canvas.py
@@ -1,69 +1,70 @@
 #!/usr/bin/env python3
-import sys, json, flask
+import sys, json, flask, math
 from queue import Queue
-from lds006 import LDSSerialManager
+from time import sleep
+import random as Random
+import msgLDS_pb2
 
-filter_array_size = 4
-min_deviation = 0.02
-max_deviation = 0.1
+app = flask.Flask(__name__, template_folder=".")
+
+# load main page
+@app.route('/')
+def index():
+    return flask.render_template("index.html")
+
+# load protobuf definitions
+@app.route('/msgLDS.proto')
+def pb():
+    return flask.render_template("msgLDS.proto")
+
+# load protobuf.js sources
+@app.route('/protobuf-7.2.2.js')
+def src_protobuf():
+    return flask.render_template("protobuf-7.2.2.js")
+# load protobuf.js.map sources
+@app.route('/protobuf.js.map')
+def src_protobuf2():
+    return flask.render_template("protobuf.js.map")
+# load update page
+@app.route('/lidar')
+def chart_data(): 
+    def generate_data():
+        while True:
+            msg = msgLDS_pb2.msgLDS()
+            data = [math.floor(Random.random()*20000) for i in range(0, 360)]
+            for i in range(360):
+                _msg = msg.data.add()
+                _msg.angle = i
+                _msg.distance = max(3000, data[i])
+                _msg.certainty = False
+                if Random.randint(0, 1) == 1:
+                    _msg.certainty = True
+
+            data = msg.SerializeToString()
+            #print(data)
+            #print("Length of data: " + str(len(data)))
+            yield f"data:{data}\n\n"
+            #json_data = json.dumps({'values': data })
+            #yield f"data:{json_data}\n\n"
+            sleep(3)
+    
+    response = flask.Response(flask.stream_with_context(generate_data()), mimetype="text/event-stream")
+    response.headers["Cache-Control"] = "no-cache"
+    response.headers["X-Accel-Buffering"] = "no"
+    return response
+
+@app.route('/start')
+def start_sensor():
+        
+    return "\n\n"
+@app.route('/pause')
+def pause_sensor():
+        
+    return "\n\n"
+@app.route('/stop')
+def stop_sensor():
+        
+    return "\n\n"
 
 if __name__ == "__main__":
-
-    # Fallback for broken library
-    # usage: python main.py /dev/serial0 stop
-    if len(sys.argv) == 3 and sys.argv[2] == "stop":
-        lds = LDSSerialManager(sys.argv[1])
-        lds.stop()
-        sys.exit(0)
-    
-    # Default program
-    # usage: python main.py /dev/serial0
-    if len(sys.argv) > 1:
-        port = sys.argv[1]
-    else:
-        port = "/dev/serial0"
-    print("Connecting to: " + port)
-    with LDSSerialManager(port) as lds:
-        app = flask.Flask(__name__)
-        cb_hash = None
-        # load main page
-        @app.route('/')
-        def index():
-            return flask.render_template("index.html")
-
-        # load update page
-        @app.route('/lidar')
-        def chart_data(): 
-            q_data = Queue(maxsize=3)
-            def get_data(x):
-                q_data.put(x)                                        
-                
-            def generate_data():
-                while True:
-                    json_data = json.dumps({'values': q_data.get() })
-                    yield f"data:{json_data}\n\n"        
-
-            global cb_hash
-            cb_hash = lds.registerCB(get_data, range(lds.NUM_OF_ENTRIES))
-            response = flask.Response(flask.stream_with_context(generate_data()), mimetype="text/event-stream")
-            response.headers["Cache-Control"] = "no-cache"
-            response.headers["X-Accel-Buffering"] = "no"
-            return response
-
-        @app.route('/start')
-        def start_sensor():
-            lds.start()
-            return "\n\n"
-        @app.route('/pause')
-        def pause_sensor():
-            lds.pause()
-            return "\n\n"
-        @app.route('/stop')
-        def stop_sensor():
-            lds.stop()
-            global cb_hash
-            lds.unregisterCB(cb_hash)
-            return "\n\n"
-
-        app.run(debug=True)
-
+    app.run(debug=True, threaded=True)
